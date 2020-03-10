272f222a0c50ef2becb8dc578ec180bc4689fd1a
Jian Mai <42229566+JianHMai@users.noreply.github.com>
2020-03-09 22:57:44 -0400

    Fix bugs and create tokenize

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import requests
 from twitterscraper import query_tweets
 import datetime as dt
 import nltk
-import pycountry
+import glob, os
+from nltk import word_tokenize
+import csv
 
 # function to retrieve list of current playing movies
 def get_recent_movies():
@@ -21,9 +23,10 @@ def get_recent_movies():
 # function to retrieve tweets
 def get_tweets(movie):
     # Starting date
-    begin_date = dt.date(2020,2,23)
+    begin_date = dt.date(2020,3,8)
+
     # Ending date
-    end_date = dt.date(2020,3,1)
+    end_date = dt.date(2020,3,9)
     limit = 1
     lang = 'english'
     
@@ -44,9 +47,24 @@ def get_tweets(movie):
         language = tc.guess_language(t.text)
 
         # If twitter is classified as English, save to the file
-        if str(pycountry.languages.get(alpha_3=language).name) == "English":
-            files.write(t.text + ',\n')
+        if str(language) == 'eng':
+            files.write(t.text + ',')
     files.close()
+
+# Function to preprocess csv file
+def preprocess(file):
+    # Open file
+    with open(file) as csv_file:
+        read_csv = csv.reader(csv_file)
+        for text in read_csv:
+            # Each row that the csv reader returns, pass it into tokenize for NLTK
+            print(word_tokenize(str(text)))
+
 
 if __name__ == '__main__':
     get_recent_movies()
+    # Location to look for CSV files
+    for file in os.listdir("C:\\Users\\Jian\\Desktop\\Movie-Tweet-Analyzer\\"):
+        if file.endswith(".csv"):
+            # Pass in file location for every CSV file found
+            preprocess(os.path.join("C:\\Users\\Jian\\Desktop\\Movie-Tweet-Analyzer\\", file))
