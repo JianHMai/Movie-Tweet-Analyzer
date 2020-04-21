10e6c9a79520b8a61965cfaf4ca21b62f975a290
Jian Mai <42229566+JianHMai@users.noreply.github.com>
2020-04-21 17:34:41 -0400

    Change dataset to include all

--- train.py
+++ train.py
@@ -8,28 +8,28 @@ import pickle
 # Function to train model using SVM
 def train():
     # List to hold data in columns
-    review = []
-    sentiment = []
+    x = []
+    y = []
 
-    url = "https://raw.githubusercontent.com/JianHMai/Movie-Tweet-Sentiment-Analysis/master/train.csv"
+    url = "https://raw.githubusercontent.com/JianHMai/Movie-Tweet-Sentiment-Analysis/master/dataset.csv"
     # Retrieve csv file from URL
     csvreader = csv.reader(codecs.iterdecode(request.urlopen(url),'utf-8'))
     # Skip header row
     next(csvreader)
     for row in csvreader:
         # Add review to list 
-        review.append(row[0])
+        x.append(row[0])
         # Add sentiment to list with 1 given positive and 0 given negative
         if row[1] == 'positive':
-            sentiment.append(1)
-        else: sentiment.append(0)
+            y.append(1)
+        else: y.append(0)
 
     # Vectorization
     vectorizer = TfidfVectorizer(stop_words='english')
-    features = vectorizer.fit_transform(review).toarray()
+    features = vectorizer.fit_transform(x).toarray()
 
     # Implement Linear SVM            
-    model = svm.LinearSVC().fit(features,sentiment)
+    model = svm.LinearSVC().fit(features,y)
     # Save model 
     pickle.dump(model, open('model.sav', 'wb'))
 
