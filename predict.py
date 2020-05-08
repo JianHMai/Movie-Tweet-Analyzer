4ad2fa0f5cd24c742057d4cffe9354b1d8c7903d
Jian Mai <42229566+JianHMai@users.noreply.github.com>
2020-05-07 22:06:43 -0400

    Create predictions with model

---
+++ predict.py
@@ -0,0 +1,17 @@
+import joblib
+import urllib.request as request
+import csv
+
+def predict():
+    # Retrieve model from link
+    #model = "https://github.com/JianHMai/Movie-Tweet-Sentiment-Analysis/blob/master/model.sav?raw=true"
+    # Save it on computer
+    #request.urlretrieve(model, "model.sav")
+    loaded_model = joblib.load("model.sav")
+    
+    files = csv.reader(open('1917Movie_new.csv', encoding = 'utf8'), delimiter=',')
+    for line in files:
+        return(loaded_model.predict(line)[0])
+
+if __name__ == '__main__':
+    print(predict())
