2b8469a8cccaeb338143fc8721dbadbd896874c1
Jian Mai <42229566+JianHMai@users.noreply.github.com>
2020-05-08 22:17:42 -0400

    Update to return sentiment of whole document

--- predict.py
+++ predict.py
@@ -3,6 +3,7 @@ import urllib.request as request
 import csv
 
 def predict():
+    total = 0
     # Retrieve model from link
     #model = "https://github.com/JianHMai/Movie-Tweet-Sentiment-Analysis/blob/master/model.sav?raw=true"
     # Save it on computer
@@ -10,9 +11,16 @@ def predict():
     loaded_model = joblib.load("model.sav")
     
     files = csv.reader(open('1917Movie_new.csv', encoding = 'utf8'), delimiter=',')
+    # Iterate through each review in CSV
     for line in files:
         # Using model to predict Tweeet positive or negative sentiment
-        return(loaded_model.predict(line)[0])
+        sentiment = loaded_model.predict(line)
+    # Iterate through list containing sentiment for each review 
+    for num in sentiment:
+        # Collect total of the whole document
+        total += num
+    # Return the document sentiment
+    return (total/len(sentiment))
 
 if __name__ == '__main__':
     print(predict())
